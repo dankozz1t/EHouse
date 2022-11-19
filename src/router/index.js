4cb9f8510f6dffa32865d42fb11b1a2a9440b056
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-11-19 19:51:15 +0200

    add reject for login page when user is logged in

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import store from "@/store";
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
@@ -52,6 +53,24 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = store.getters["auth/isLoggedIn"];
+
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!isLoggedIn) {
+      next({ name: "login" });
+    }
+  }
+
+  if (to.matched.some((record) => record.meta.hideForAuth)) {
+    if (isLoggedIn) {
+      next({ name: "home" });
+    }
+  }
+
+  next();
 });
 
 export default router;
